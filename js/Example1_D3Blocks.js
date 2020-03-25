407902f008adcbc6ddba28ffdfa63d93c9b788e9
jx-qqq <58958817+jx-qqq@users.noreply.github.com>
2020-03-24 23:49:55 -0500

    Lesson 2

--- js/Example1_D3Blocks.js
+++ js/Example1_D3Blocks.js
@@ -15,6 +15,15 @@ window.onload = function(){
 
   //innerRect block
   var innerRect = container.append("rect") //put a new rect in the svg
-      .attr("width", 800) //rectangle width
-      .attr("height", 400) //rectangle height
+      .datum(400) //a single value is a DATUM
+      .attr("width", function(d){ //rectangle width
+          return d * 2; //400 * 2 = 800
+      })
+      .attr("height", function(d){ //rectangle height
+          return d; //400
+      })
+      .attr("class", "innerRect") //class name
+      .attr("x", 50) //position from left on the x (horizontal) axis
+      .attr("y", 50) //position from top on the y (vertical) axis
+      .style("fill", "#FFFFFF"); //fill color
 }
